17040ef07fcfe0f8dcd908698148afa17407e014
Nercy99 <51016318+Nercy99@users.noreply.github.com>
2019-10-21 14:34:36 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title>&Agrave; propos</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>  
+		<!-- comment -->
+		<header class="group">
+			<h1 class="logo">Psitech</h1>
+			<img src="logoapp.PNG" width="100" height="100"/>
+			<h4>&Agrave; propos</h4>
+	    </header>
+
+	    <div class="group">
+	    	<header>
+	   			<?php include("menus.php"); ?>
+
+	    	</header>
+	  		<section>
+	    		<h1>Bloc 1</h1>
+	   		</section>
+
+
+		</div>
+
+		<section>
+	    	<h1>Bloc 2</h1>
+	    </section>
+
+	    <footer class="group">
+			<small>&copy; Psitech</small>
+	    </footer>
+
+
+	</body>
+
+</html>
