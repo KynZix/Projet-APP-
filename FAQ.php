96e95896eb656076660b5ae934d6e332aa251440
Nercy99 <51016318+Nercy99@users.noreply.github.com>
2019-10-21 14:24:01 +0200

    Add files via upload

---
+++ FAQ.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title>FAQ</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>  
+		<!-- comment -->
+		<header class="group">
+			<h1 class="logo">Psitech</h1>
+	    </header>
+
+	    <div class="group">
+	    	<header>
+	   			<?php include("menus.php"); ?>
+	    	</header>
+	  		<section>
+	  		<h4>FAQ</h4>
+			<dl>
+				<dt>Q1</dt>
+				<dd>R1</dd>
+				<dt>Q2</dt>
+  				<dd>R2</dd>
+			</dl>
+	   		</section>
+
+		</div>
+
+		<section>
+	    	<h1>Bloc 2</h1>
+	    </section>
+
+	    <footer class="group">
+			<small>&copy; Psitech</small>
+	    </footer>
+
+
+	</body>
+
+</html>
