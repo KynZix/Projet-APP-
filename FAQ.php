1a213747d64c2717b1ff98d69dbefdce88a4b4d8
depotoire <56601378+depotoire@users.noreply.github.com>
2019-11-26 16:40:44 +0100

    Add files via upload

--- FAQ.php
+++ FAQ.php
@@ -4,37 +4,47 @@
 
 	<head>
 		<title>FAQ</title>
-		<link rel="stylesheet" href="CSS/faq.css">
+		<link rel="stylesheet" href="CSS/main.css">
 	</head>
 
 	<body>  
-	<?php include("header.php"); ?>
-	<p>
-		<div id="conteneur">
-			<div class="element1">
-				<!-- Cette partie sert à positionner la FAQ et la recherche -->
-					<h1>FAQ</h1> 
-					<form action="faq.php" method = "post">
-						Search : <input type="text" name="question" /><br/>
-					<input type="submit" name="submit">
-					</form>
-			</div>
-			<div class="element2">
-				<!-- Ici on va avoir les questions/réponses -->
-				<h6>Comment faire pour avoir un compte?</h6>
-			</div>
-			<div class="element2">
-				Je sais comment le faire ! 
-			</div>
-			
-		</div>
-	</p>	
-
+		<!-- comment -->
 		
 
+		<body>
+			<header>
+				<?php include("header.php"); ?>
+			</header>
 
+			<?php
+			//on essaie e se connecter a la bdd, s'il y a une erreur on affiche juste une erreur au lieu d'afficher le lien vers la bdd par securité
+			try
+			{
+				$bdd = new PDO('mysql:host=localhost;dbname=app;charset=utf8', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+			}
+			catch(Exception $e)
+			{
+				die('erreur:'.$e -> getMessage());
+			}
+			?>
+			
 
-	<?php include("footer.php"); ?>
+			<?php
+			//on recupere les 10 dernieres questions pseudos et reponses dans une table
+			$questionsPseudosReponses = $bdd->query('SELECT question, reponse FROM faq ORDER BY id DESC LIMIT 0,10');
+
+			while ($questionPseudoReponse = $questionsPseudosReponses->fetch())
+			{
+				echo '<section>' . $questionPseudoReponse['question'] . ':</n> ' . $questionPseudoReponse['reponse'];
+			}
+			?>
+		
+		
+			<footer>
+				<?php include("footer.php"); ?>
+			</footer>
+		</body>
+	
 	    
 
 
