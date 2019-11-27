9cdb3883ebbdb6996439b04aecb162e1e849be25
Mecar1997 <56541507+Mecar1997@users.noreply.github.com>
2019-11-27 11:27:24 +0100

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title>FAQ</title>
+		<link rel="stylesheet" href="CSS/main.css">
+	</head>
+
+	<body>  
+		<!-- comment -->
+		
+
+		<body>
+			<header>
+				<?php include("header.php"); ?>
+			</header>
+
+
+		
+		
+			<footer>
+				<?php include("footer.php"); ?>
+			</footer>
+		</body>
+	
+	    
+
+
+	</body>
+
+</html>
