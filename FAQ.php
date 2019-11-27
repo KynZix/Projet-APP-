23255016ff4b9b96ac0cd660aeca3d005e8a6aa9
depotoire <56601378+depotoire@users.noreply.github.com>
2019-11-27 11:21:19 +0100

    Add files via upload

--- FAQ.php
+++ FAQ.php
@@ -4,99 +4,91 @@
 
 	<head>
 		<title>FAQ</title>
-		<link rel="stylesheet" href="CSS/faq.css">
+		<link rel="stylesheet" href="CSS/main.css">
 	</head>
 
 	<body>  
-	<?php include("header.php"); ?>
-	<p>
-		<div id="conteneur">
-			<div class="element1">
-				<!-- Cette partie sert à positionner la FAQ et la recherche -->
-					<h1>FAQ</h1> 
-					<form action="faq.php" method = "post">
-						Recherche : <input type="text" name="question" /><br/>
-					<input type="submit" name="submit">
-					</form>
-			</div>
-			<!-- 1ère question -->
-			<div class="element2">
-				<!-- Ici on va avoir les questions/réponses -->
-				<span class="span1" tabindex="0">Comment faire pour avoir un compte?</span>
-				<p class="text1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-			</div>
-			<!-- 2ème question -->
-			<div class="element2">
-				<span class="span1" tabindex="0">Comment avoir un superpouvoir?</span>
-				<p class="text2">Je sais pas . </p>
-			</div>
-			<!-- 3ème question -->
-			<div class="element2">
-				<span class="span1" tabindex="0">Comment fait Cyril pour être si beau ?</span>
-				<p class="text3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.. </p>
-			</div>
-			<!-- 4ème question -->
-			<div class="element2">
-				<span class="span1" tabindex="0">Comment avoir 20/20?</span>
-				<p class="text4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.. </p>
-			</div>
-			<!-- 5ème question -->
-			<div class="element2">
-				<span class="span1" tabindex="0">Comment être intelligent ?</span>
-				<p class="text5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.. </p>
-			</div>
-		</div>
-		<!-- 6ème question -->
-			<div class="element2">
-				<span class="span1" tabindex="0">Comment ne pas perdre ses cheveux ?</span>
-				<p class="text6">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.. </p>
-			</div>
-		</div>
-
-		<!-- 7ème question -->
-			<div class="element2">
-				<span class="span1" tabindex="0">Comment faire si j'ai renversé du coca sur ma machine ?</span>
-				<p class="text7">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.. </p>
-			</div>
-		</div>
-	</p>	
-
+		<!-- comment -->
 		
 
+		<body>
+			<!--
+			<header>
+				<?php //include("header.php"); ?>
+			</header>
+			-->
+
+			<?php
+			//on essaie e se connecter a la bdd, s'il y a une erreur on affiche juste une erreur au lieu d'afficher le lien vers la bdd par securité
+			try
+			{
+				$bdd = new PDO('mysql:host=localhost;dbname=app;charset=utf8', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+			}
+			catch(Exception $e)
+			{
+				die('erreur:'.$e -> getMessage());
+			}
+			?>
 
 
-	<?php include("footer.php"); ?>
+			<div id="conteneur">
+				<div class="element1">
+					<!-- Cette partie sert à positionner la FAQ et la recherche -->
+						<h1>FAQ</h1> 
+						<form action="faq.php" method = "post">
+							Recherche : <input type="text" name="question" /><br/>
+						<input type="submit" name="submit">
+						</form>
+				</div>
+
+
+
+				<?php
+				//on recupere les 10 dernieres questions pseudos et reponses dans une table
+				$questionsReponses = $bdd->query('SELECT question, reponse FROM faq ORDER BY id DESC');
+
+				while ($questionReponse = $questionsReponses->fetch()){
+				?>
+				<!-- questions/reponses -->
+					<div class="element2">
+
+						<!-- question -->
+						<span class="span1" tabindex="0">
+							<?php echo $questionReponse['question']; ?>
+						</span>
+
+						<!-- reponses-->
+						<p class="text1">
+							<?php echo $questionReponse['reponse']?>
+						</p>
+					</div>
+				<?php } ?>
+			</div>
+
+				
+				
+					
+					 
+					
+				
+
+			<?php
+			//recuperer le nombre de question
+			/*
+			$nrbQuestions =  $bdd->query('SELECT MAX(id) AS nbr FROM faq');
+			$nbrQuestions = $nrbQuestions->fetch();
+			$nbrQuestions = $nbrQuestions['nbr'];
+			echo $nbrQuestions;
+			*/
+			?>	
+
+			<!--
+			<footer>
+				<?php include("footer.php"); ?>
+			</footer>
+			-->
+		</body>
+	
 	    
 
 
