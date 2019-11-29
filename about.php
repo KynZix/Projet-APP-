e09c87920f35173732237a95de162d73a2f7a074
KynZix <56548224+KynZix@users.noreply.github.com>
2019-11-29 13:58:21 +0100

    Add files via upload

--- about.php
+++ about.php
@@ -4,27 +4,40 @@
 
 	<head>
 		<title>&Agrave; propos</title>
-		<link rel="stylesheet" href="CSS/main.css">
+		<link rel="stylesheet" href="CSS/about.css">
 	</head>
 
 	<body>  
+		<header>
+			 <?php include("header.php"); ?>
+		</header>
+	  
+	  <p> 
+	  	<div class="about">
+	  		<img src="Media/about.png">
+	  	</div>
+	  </p>
 
-	   <?php include("menus.php"); ?>
+	   <p>
+      	<div id="conteneur">
+      		<div class="element">
+      			<img src="Media/team.png" >
+      		</div>
+      		<div class="element">
+      			<p>Founded in Paris in 2019, Psitech was created as an answer to the luxury trend of over-abundant branding and excessive pricing.
+      			</p>
+      			<p>Our designs do not confine to fashion - we simply aspire to provide high quality, well-designed essentials for the right price. 
+      			</p>
+      			<p>We work with a minimalistic philosophy, as we believe sustainability and durability are better for us and our world.
+      			</p>
+      		</div>
+      	</div>
+      </p>
 
-	   <div class="group"> <!-- body -->
-	  		<section>
-	    		<h1></h1>	
-	   	</section>
-	   	<section>
-	    		<h1></h1>
-	   	</section>
-	   	<section>
-	    		<h1></h1>
-	   	</section>
-		</div>
-		
-		<!-- footer -->
-	  	<?php include("footer.php"); ?>
+		<footer>
+			<?php include("footer.php"); ?>
+		</footer>
+	  	
 	</body>
 
 </html>
