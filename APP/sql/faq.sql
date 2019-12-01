3606e17e583ab6a3f5a09cc8ffdcac1b300338d4
Mecar1997 <56541507+Mecar1997@users.noreply.github.com>
2019-12-01 19:56:25 +0100

    Add files via upload

--- APP/sql/faq.sql
+++ APP/sql/faq.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 01 déc. 2019 à 14:16
+-- Généré le :  Dim 01 déc. 2019 à 18:56
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -30,27 +30,32 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `faq`;
 CREATE TABLE IF NOT EXISTS `faq` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `question` text NOT NULL,
   `reponse` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=244 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `faq`
 --
 
 INSERT INTO `faq` (`id`, `question`, `reponse`) VALUES
-(1, 'question1 ?', 'reponse 1\r\n'),
-(2, 'question2', 'reponse2\r\n'),
-(3, 'question3', 'reponse3'),
-(4, 'question4', 'reponse4'),
-(5, 'question5', 'reponse5\r\n'),
-(6, 'question6', 'reponse6'),
-(7, 'question7', 'reponse7'),
-(8, 'question8', 'question8'),
-(9, 'question9', 'reponse9\r\n'),
-(10, 'question10', 'reponse10');
+(1, 'Comment utiliser l\'appareil ?', 'Réponse 1'),
+(2, 'Question 2', 'Réponse 2'),
+(3, 'Comment créer un compte ?', 'Il est actuellement impossible de manuellement créer son propre compte. Pour en avoir un, il faut se rendre dans l\'un des centres Psitech. Un gestionnaire se chargera de créer un compte en fonction des données que lui vous fournissez, et le mot de passe vous sera envoyé par mail.'),
+(4, 'Question 4', 'Réponse 4'),
+(5, 'Question 5', 'Réponse 5'),
+(6, 'Question 6', 'Réponse 6'),
+(7, 'Question 7', 'Réponse 7'),
+(8, 'Question 8', 'Réponse 8'),
+(9, 'Question 9', 'Réponse 9'),
+(10, 'Question 10', 'Réponse 10'),
+(11, 'Question 11', 'Réponse 11'),
+(12, 'Question 12', 'Réponse 12'),
+(13, 'Question 13', 'Réponse 13'),
+(14, 'Question 14', 'Réponse 14'),
+(15, 'Question 15', 'Réponse 15');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
