a0a6e532debe92d60b892685660b35a3cb0b0559
Nercy99 <51016318+Nercy99@users.noreply.github.com>
2019-12-01 15:18:50 +0100

    Add files via upload

--- APP/register.php
+++ APP/register.php
@@ -1,102 +1,114 @@
+<?php Session_start();
+$_SESSION['nom'] = "";
+$_SESSION['prenom'] = "";
+$_SESSION['mail'] ="";
+$_SESSION['dateDeNaissance'] ="";
+$_SESSION['numeroDeTelephone'] ="";
+$_SESSION['sexe'] ='';
+$_SESSION['pays'] ='';
+$_SESSION['ville'] ='';
+$_SESSION['codePostal'] ='';
+$_SESSION['adresse'] ='';
+$_SESSION['complementAdresse'] ='';
+?>
+
 <!DOCTYPE html>
 
 <html lang="fr">
 
 	<head>
-		<title>Créer un compte</title>
-		<link rel="stylesheet" href="CSS/register.css">
+		<title>Cr&eacute;er un compte</title>
+		<link rel="stylesheet" href="CSS/main.css">
 	</head>
 
 	<body> 
-
-		<?php include("header.php"); ?>
+		<header>
+			<?php include("header.php"); ?>
+		</header>
 		
-	    <div>
+	    <div class="group">
 	  		<section>
+	  				
 	  			<form name="inscription" method="post" action="saisie.php">
-			  		<legend><p>Creation d'un nouveau compte</p></legend>
-			  		<div class="formulaire">
+	  				
+		  			<fieldset class="account_info">
+			  			<legend>Cr&eacute;ation d'un nouveau compte</legend>
+					</fieldset>
+					<fieldset class="account_info">
+						<label>
+							<link rel = "stylesheet" href="CSS/saisie.css">
+							Adresse mail <input type="text" name="mail" id="mail" placeholder="abcd123@gmail.com"> 
+						</label>
+						<label>
+						<link rel = "stylesheet" href="CSS/saisie.css"> 
+							Pr&eacute;nom <input type="text" name="first_name" id="first_name" placeholder="ab">
+						</label>
+						<label> 
+							Nom <input type="text" name="last_name" id="last_name" placeholder="cd">
+						</label>
 
-			  			<fieldset>
-							<label>
-								<p>Nom</p>
-								<input type="text" name="Nom" id="name" placeholder="Nom" required>
-							</label>
-							<label>
-								<p>Prenom</p>
-								<input type="text" name="Prenom" id="prenom" placeholder="Prenom" required>
-							</label>
-						</fieldset>
+					</fieldset>
+					
+					<fieldset class="account_info">
+						<label> 
+							<link rel = "stylesheet" href="CSS/saisie.css">
+							Date de naissance <input type="date" name="birthday" id="birthday" placeholder="10/00/1997">
+						</label>
+						<label>
+						<link rel = "stylesheet" href="CSS/saisie.css"> 
+							Num&eacute;ro <input type="number" name="phone" id="phone" placeholder="1234657890">
+						</label>
+					</fieldset>
+					
+					
+					<fieldset class="account_info">
+						<label>
+							<link rel = "stylesheet" href="CSS/saisie.css">
+							Nom <input type="text" name="name" id="name" placeholder="Nom">
+						</label>
+						<label>
+							<link rel = "stylesheet" href="CSS/saisie.css">
+							Prenom <input type="text" name="prenom" id="prenom" placeholder="Prenom">
+						</label>
+						<label>
+							<link rel = "stylesheet" href="CSS/saisie.css">
+							Sexe <input type="text" name="sex" id="sex" placeholder="Sexe">
+						</label>
+						<label>
+						<link rel = "stylesheet" href="CSS/saisie.css"> 
+							Pays <input type="text" name="country" id="country" placeholder="France">
+						</label>
 
-						<fieldset>
-							<label><input type="radio" name="gender" value="male" required>Male</label>
-							<label><input type="radio" name="gender" value="female">Female</label>
-							<label><input type="radio" name="gender" value="other">Other</label>
-						</fieldset>
+						<label>
+						<link rel = "stylesheet" href="CSS/saisie.css"> 
+							Ville <input type="text" name="town" id="town" placeholder="Ville">
+						</label>
 
-						<fieldset>
-							<label> 
-								<p>Date de naissance</p>
-								<input type="date" name="date de naissance" id="birthday" placeholder="10/00/1997" required>
-							</label>
-						</fieldset>	
+						<label>
+						<link rel = "stylesheet" href="CSS/saisie.css"> 
+							Code Postal <input type="number" name="ZIP" id="ZIP" placeholder="Code Postal">
+						</label>
+					</fieldset>
 
-						<fieldset>
-							<label>
-								<p>Adresse mail</p>
-								<input type="text" name="mail" id="mail" placeholder="abcd123@gmail.com" required> 
-							</label>
-						</fieldset>
+					<fieldset class="account_info">
+						<label> 
+							<link rel = "stylesheet" href="CSS/saisie.css">
+							Adresse <input type="text" name="adress" id="adress" placeholder="Adresse">
+						</label>
+						<label>
+							<link rel = "stylesheet" href="CSS/saisie.css"> 
+							Compl&eacute;ment d'adresse <input type="text" name="adress2" id="adress2" placeholder="Complement d adresse">
+						</label>
+						<label>
+							<link rel = "stylesheet" href="CSS/saisie.css">
+							type utilisateur <input type="text" name="typeUtilisateur" id="typeUtilisateur" placeholder="type d'utilisateur">
+						</label>
+					</fieldset>
 
-						<fieldset >	
-							<label> 
-								<p>Numero</p>
-								<input type="number" name="Numero" id="phone" placeholder="1234657890" required>
-							</label>
-						</fieldset>
-
-						<fieldset>
-							<label> 
-								<p>Pays</p>
-								<input type="text" name="Pays" id="country" placeholder="France" required>
-							</label>
-							<label> 
-								<p>Ville</p>
-								<input type="text" name="Ville" id="town" placeholder="Ville" required>
-							</label>
-							<label> 
-								<p>Code Postal</p>
-								<input type="number" name="Code Postal" id="ZIP" placeholder="Code Postal" required>
-							</label>
-						</fieldset>
-
-						<fieldset >
-							<label>
-								<p>Adresse</p>
-								<input type="text" name="Adresse" id="adress" placeholder="Adresse" required>
-							</label>
-							<label> 
-								<p>Complement d'adresse</p>
-								<input type="text" name="Complement d'adresse" id="adress2" placeholder="Complement d'adresse">
-							</label>
-						</fieldset>
-					</div>
 					<input type="submit" name="submit" value="S'inscrire">
 
 				</form>
 				
-				<?php
-				$subject = "inscription sur le site Psytech";
-				$message = "répondez à ce message si vous avez bien demandé à être inscrit aux tests psychotechniques";
-				$headers = array(
-		    		'From' => 'cyril.nerin@gamil.com',
-		    		'CC ' => 'leonard.huang@isep.fr, msaknisabri@gmail.com, cdechateauvieux7@gmail.com, leopold.lacruz@gmail.com, leonard.huang.99@gmail.com', 
-		    		'Reply-To' => 'cyril.nerin@gmail.com, leonard.huang@isep.fr, msaknisabri@gmail.com, cdechateauvieux7@gmail.com, leopold.lacruz@gmail.com, leonard.huang.99@gmail.com',
-		    		'X-Mailer' => 'PHP/' . phpversion()
-				);
-
-				?>
-
 	   		</section>
 
 
