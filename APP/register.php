1f7d0cf02af241da04114b720bff5f18e6e9ae3d
depotoire <56601378+depotoire@users.noreply.github.com>
2019-12-01 17:31:05 +0100

    Add files via upload

--- APP/register.php
+++ APP/register.php
@@ -1,114 +1,148 @@
-<?php Session_start();
-$_SESSION['nom'] = "";
-$_SESSION['prenom'] = "";
-$_SESSION['mail'] ="";
-$_SESSION['dateDeNaissance'] ="";
-$_SESSION['numeroDeTelephone'] ="";
-$_SESSION['sexe'] ='';
-$_SESSION['pays'] ='';
-$_SESSION['ville'] ='';
-$_SESSION['codePostal'] ='';
-$_SESSION['adresse'] ='';
-$_SESSION['complementAdresse'] ='';
-?>
-
 <!DOCTYPE html>
 
 <html lang="fr">
 
 	<head>
-		<title>Cr&eacute;er un compte</title>
-		<link rel="stylesheet" href="CSS/main.css">
+		<title>Créer un compte</title>
+		<link rel="stylesheet" href="CSS/register.css">
 	</head>
 
 	<body> 
-		<header>
-			<?php include("header.php"); ?>
-		</header>
+
+		<?php include("header.php"); ?>
 		
-	    <div class="group">
+	    <div>
 	  		<section>
-	  				
-	  			<form name="inscription" method="post" action="saisie.php">
-	  				
-		  			<fieldset class="account_info">
-			  			<legend>Cr&eacute;ation d'un nouveau compte</legend>
-					</fieldset>
-					<fieldset class="account_info">
-						<label>
-							<link rel = "stylesheet" href="CSS/saisie.css">
-							Adresse mail <input type="text" name="mail" id="mail" placeholder="abcd123@gmail.com"> 
-						</label>
-						<label>
-						<link rel = "stylesheet" href="CSS/saisie.css"> 
-							Pr&eacute;nom <input type="text" name="first_name" id="first_name" placeholder="ab">
-						</label>
-						<label> 
-							Nom <input type="text" name="last_name" id="last_name" placeholder="cd">
-						</label>
+	  			<form name="inscription" method="post" action="registerTest.php">
+			  		<legend><p>Creation d'un nouveau compte</p></legend>
+			  		<div class="formulaire">
 
-					</fieldset>
-					
-					<fieldset class="account_info">
-						<label> 
-							<link rel = "stylesheet" href="CSS/saisie.css">
-							Date de naissance <input type="date" name="birthday" id="birthday" placeholder="10/00/1997">
-						</label>
-						<label>
-						<link rel = "stylesheet" href="CSS/saisie.css"> 
-							Num&eacute;ro <input type="number" name="phone" id="phone" placeholder="1234657890">
-						</label>
-					</fieldset>
-					
-					
-					<fieldset class="account_info">
-						<label>
-							<link rel = "stylesheet" href="CSS/saisie.css">
-							Nom <input type="text" name="name" id="name" placeholder="Nom">
-						</label>
-						<label>
-							<link rel = "stylesheet" href="CSS/saisie.css">
-							Prenom <input type="text" name="prenom" id="prenom" placeholder="Prenom">
-						</label>
-						<label>
-							<link rel = "stylesheet" href="CSS/saisie.css">
-							Sexe <input type="text" name="sex" id="sex" placeholder="Sexe">
-						</label>
-						<label>
-						<link rel = "stylesheet" href="CSS/saisie.css"> 
-							Pays <input type="text" name="country" id="country" placeholder="France">
-						</label>
+			  			<fieldset>
+							<label>
+								<p>Nom</p>
+								<input type="text" name="nom" id="name" placeholder="Nom" 
+									<?php if (isset($_COOKIE['nom'])) {
+										echo 'value='.$_COOKIE['nom'];
+									} ?>
+									required>
+							</label>
+							<label>
+								<p>Prenom</p>
+								<input type="text" name="prenom" id="prenom" placeholder="Prenom"
+									<?php if (isset($_COOKIE['prenom'])) {
+										echo 'value=' .$_COOKIE['prenom'];
+									} ?> 
+									required>
+							</label>
+						</fieldset>
 
-						<label>
-						<link rel = "stylesheet" href="CSS/saisie.css"> 
-							Ville <input type="text" name="town" id="town" placeholder="Ville">
-						</label>
+						<fieldset>
+							<label><input type="radio" name="genre" value="1" required 
+								<?php if (isset($_COOKIE['genre']) and $_COOKIE['genre']==1) { echo "checked";} ?> 
+								>Male</label>
+							<label><input type="radio" name="genre" value="0"
+								<?php if (isset($_COOKIE['genre']) and $_COOKIE['genre']==0) { echo "checked";} ?> 
+								>Female</label>
+							<label><input type="radio" name="genre" value="2"
+								<?php if (isset($_COOKIE['genre']) and $_COOKIE['genre']==2) { echo "checked";} ?>
+								>Other</label>
+						</fieldset>
 
-						<label>
-						<link rel = "stylesheet" href="CSS/saisie.css"> 
-							Code Postal <input type="number" name="ZIP" id="ZIP" placeholder="Code Postal">
-						</label>
-					</fieldset>
+						<fieldset>
+							<label> 
+								<p>Date de naissance</p>
+								<input type="date" name="birthday" id="birthday" placeholder="10/00/1997"
+									<?php if (isset($_COOKIE['birthday'])) {
+										echo 'value=' .$_COOKIE['birthday'];
+									} ?>
+									required>
+							</label>
+						</fieldset>	
 
-					<fieldset class="account_info">
-						<label> 
-							<link rel = "stylesheet" href="CSS/saisie.css">
-							Adresse <input type="text" name="adress" id="adress" placeholder="Adresse">
-						</label>
-						<label>
-							<link rel = "stylesheet" href="CSS/saisie.css"> 
-							Compl&eacute;ment d'adresse <input type="text" name="adress2" id="adress2" placeholder="Complement d adresse">
-						</label>
-						<label>
-							<link rel = "stylesheet" href="CSS/saisie.css">
-							type utilisateur <input type="text" name="typeUtilisateur" id="typeUtilisateur" placeholder="type d'utilisateur">
-						</label>
-					</fieldset>
+						<fieldset>
+							<label>
+								<p>Adresse mail</p>
+								<input type="text" name="mail" id="mail" placeholder="abcd123@gmail.com"
+									<?php if (isset($_COOKIE['mail'])) {
+										echo 'value=' .$_COOKIE['mail'];
+									} ?>
+								required> 
+							</label>
+						</fieldset>
 
+						<fieldset >	
+							<label> 
+								<p>Numero</p>
+								<input type="number" name="phone" id="phone" placeholder="1234657890"
+									<?php if (isset($_COOKIE['phone'])) {
+										echo 'value=' .$_COOKIE['phone'];
+									} ?>
+									required>
+							</label>
+						</fieldset>
+
+						<fieldset>
+							<label> 
+								<p>Pays</p>
+								<input type="text" name="pays" id="country" placeholder="France"
+								<?php if (isset($_COOKIE['pays'])) {
+										echo 'value=' .$_COOKIE['pays'];
+									} ?>
+									required>
+							</label>
+							<label> 
+								<p>Ville</p>
+								<input type="text" name="ville" id="town" placeholder="Ville"
+								<?php if (isset($_COOKIE['ville'])) {
+										echo 'value=' .$_COOKIE['ville'];
+									} ?>
+									required>
+							</label>
+							<label> 
+								<p>Code Postal</p>
+								<input type="number" name="ZIP" id="ZIP" placeholder="Code Postal"
+								<?php if (isset($_COOKIE['ZIP'])) {
+										echo 'value=' .$_COOKIE['ZIP'];
+									} ?>
+									required>
+							</label>
+						</fieldset>
+
+						<fieldset >
+							<label>
+								<p>Adresse</p>
+								<input type="text" name="adresse" id="adress" placeholder="Adresse"
+								<?php if (isset($_COOKIE['adresse'])) {
+										echo 'value=' .$_COOKIE['adresse'];
+									} ?>
+									required>
+							</label>
+							<label> 
+								<p>Complement d'adresse</p>
+								<input type="text" name="adresse2" id="adress2" placeholder="Complement d'adresse"
+								<?php if (isset($_COOKIE['adresse2'])) {
+										echo 'value=' .$_COOKIE['adresse2'];
+									} ?>
+									required>
+							</label>
+						</fieldset>
+					</div>
 					<input type="submit" name="submit" value="S'inscrire">
 
 				</form>
 				
+				<?php
+				$subject = "inscription sur le site Psytech";
+				$message = "répondez à ce message si vous avez bien demandé à être inscrit aux tests psychotechniques";
+				$headers = array(
+		    		'From' => 'cyril.nerin@gamil.com',
+		    		'CC ' => 'leonard.huang@isep.fr, msaknisabri@gmail.com, cdechateauvieux7@gmail.com, leopold.lacruz@gmail.com, leonard.huang.99@gmail.com', 
+		    		'Reply-To' => 'cyril.nerin@gmail.com, leonard.huang@isep.fr, msaknisabri@gmail.com, cdechateauvieux7@gmail.com, leopold.lacruz@gmail.com, leonard.huang.99@gmail.com',
+		    		'X-Mailer' => 'PHP/' . phpversion()
+				);
+
+				?>
+
 	   		</section>
 
 
---
+++ APP/registerTest.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title> Bienvenue</title>
+	<link rel="stylesheet" href="CSS/main.css">
+</head>
+<body>
+	<?php include("header.php"); ?>
+	<section>
+		<h1> Cr&eacute;ation du compte </h1>
+		<p> Voici les informations saisies : </p>
+		<p>
+			<link rel = "stylesheet" href="CSS/saisie.css">
+
+			<?php
+				//on essaie e se connecter a la bdd, s'il y a une erreur on affiche juste une erreur au lieu d'afficher le lien vers la bdd par securité
+				try
+				{
+					$bdd = new PDO('mysql:host=localhost;dbname=app;charset=utf8', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+				}
+				catch(Exception $e)
+				{
+					die('erreur:'.$e -> getMessage());
+				}
+			?>
+			<?php
+			//on remplis les infos
+			$nom = $_POST['nom'];
+			$prenom = $_POST['prenom'];
+			$genre = $_POST['genre'];
+			$birthday = $_POST['birthday'];
+			$mail = $_POST['mail'];
+			$phone = $_POST['phone'];
+			$pays = $_POST['pays'];
+			$ville = $_POST['ville'];
+			$ZIP = $_POST['ZIP'];
+			$adresse = $_POST['adresse'];
+
+			if(isset($_POST['adresse2']))
+			{
+				$adresse2 = $_POST['adresse2'];
+			}
+			else
+			{
+				$adresse2 = 'NR';
+			}
+
+			$typeUtilisateur = 2;
+
+			echo 'mail =', $mail, '='; 
+			echo "<br>";
+			echo 'birthday =', $birthday, '='; 
+			echo "<br>";
+			echo 'phone =', $phone, '='; 
+			echo "<br>";
+			echo 'nom =', $nom, '='; 
+			echo "<br>";
+			echo 'prenom =', $prenom, '='; 
+			echo "<br>";
+			echo 'genre =', $genre, '='; 
+			echo "<br>";
+			echo 'pays =', $pays, '='; 
+			echo "<br>";
+			echo 'ville =', $ville, '='; 
+			echo "<br>";
+			echo 'ZIP =', $ZIP, '='; 
+			echo "<br>";
+			echo 'adresse =', $adresse, '='; 
+			echo "<br>";
+			echo 'adresse2 =', $adresse2, '='; 
+			echo "<br>";
+			echo 'typeUtilisateur =', $typeUtilisateur, '='; 
+			echo "<br>";
+
+			//on cherche le mail dans la base de donné
+			$req1 = $bdd->prepare('SELECT mail FROM compte WHERE mail=:mail');
+			$req1 -> execute(array('mail' => $mail));
+			$mailBDD = $req1->fetch();
+
+			//on test si mail est deja present pour eviter les erreurs
+			if (isset($mailBDD['mail'])) {
+				echo "<a href = \"register.php\">no chance</a>";
+				header('Location: register.php');
+				setcookie('nom',$nom,time() + 600);
+				setcookie('prenom',$prenom,time() + 600);
+				setcookie('genre',$genre,time() + 600);
+				setcookie('birthday',$birthday,time() + 600);
+				setcookie('phone',$phone,time() + 600);
+				setcookie('pays',$pays,time() + 600);
+				setcookie('ville',$ville,time() + 600);
+				setcookie('ZIP',$ZIP,time() + 600);
+				setcookie('adresse',$adresse,time() + 600);
+				if(isset($adresse2))
+				{
+					setcookie('adresse2',$adresse2,time() + 600);
+				}
+				
+			}
+			//s'il n'est pas present on ajoute les infos et on redirige a lindex
+			else
+			{
+			$req = $bdd->prepare(' INSERT INTO compte(mail, birthday, phone, nom, prenom, genre, pays, ville, ZIP, adresse, adresse2, typeUtilisateur) VALUES (:mail, :birthday, :phone, :nom, :prenom, :genre, :pays, :ville, :ZIP, :adresse, :adresse2, :typeUtilisateur) ');
+			$req -> execute(array(
+			'mail' => $mail,
+			'birthday'=> $birthday,
+			'phone'=> $phone,
+			'nom'=> $nom,
+			'prenom'=> $prenom,
+			'genre'=> $genre,
+			'pays'=> $pays,
+			'ville'=> $ville,
+			'ZIP'=> $ZIP,
+			'adresse'=> $adresse,
+			'adresse2'=> $adresse2,
+			'typeUtilisateur'=> $typeUtilisateur));
+
+			echo "<br>";
+			echo 'Votre compte a été créé avec succès!';
+			//on redirige
+			header("Location: index.php");
+			}
+			
+			?>
+			</p>
+		</section>
+	<?php include("footer.php"); ?>
+		
+</body>
