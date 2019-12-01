57c848562296b6422943f0bf5ecf8a8011b66c7a
Mecar1997 <56541507+Mecar1997@users.noreply.github.com>
2019-12-01 18:51:56 +0100

    Update header.php

--- header.php
+++ header.php
@@ -76,7 +76,7 @@
 			<div class="menu2">
 		  		<form class="menuinput"  id="leftbutton" action="FAQ.php">
 		  			<input type="search" name="searchtext" placeholder="Recherche..." id ="search-bar">	
-					<button type="submit"><i class="search-button"></i></button>
+					<button type="submit" id="search-button"><i class="fa fa-search"></i></button>
 		  		</form>
 			</div>
 
