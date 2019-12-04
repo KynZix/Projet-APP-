0fdf4776c7e4fe74d58d5ea40fb5d2b5f992ab65
KynZix <56548224+KynZix@users.noreply.github.com>
2019-12-04 09:35:13 +0100

    Add files via upload

--- APP/login.php
+++ APP/login.php
@@ -14,33 +14,52 @@
 		<?php include("header.php"); ?>
 
 
-	    <div class="group">
+		
+	    <div class="login">
+
+					<h1>Bienvenue ! Connecte toi ici </h1>
+
 	  		<section>
 
 	  			<form action="loginTest.php" id="login" method="post">
-	  				<legend>Login</legend>
+	  				<!-- <legend>Login</legend> -->
+	  			<div class="inputinfo">
+	  				
 		  			<fieldset>
 						<label>
-							Adresse mail <input type="text" name="mail" id="mail" placeholder="exemple@gmail.com" required> 
+							Adresse e-mail* <input type="text" name="mail" id="mail" placeholder="exemple@gmail.com" required> 
 						</label>
 					</fieldset>
 
 					<fieldset class="account_info">
 						<label> 
-							Mot de passe <input type="password" name="mdp" id="password" placeholder="*****" required> 
+							Mot de passe* <input type="password" name="mdp" id="password" placeholder="*****" required> 
 						</label>
 					</fieldset>
+				</div>
 
+				<div class="submitbut">
 					<fieldset class="account_info">
 						<input type="submit" name="remember" value="Se connecter" id="button">
 					</fieldset>
+				</div>
+
+					
 				</form>
-
-				
-
-
 	   		</section>
 		</div>
+		
+		<div id="loginquestion">
+			<p id="loginquestion">
+				Tu n'as pas de compte ? 
+			</p>
+			<p id="loginquestion">
+				Demande à un admin de te créer un compte <a href="contactus.php">Juste ici !</a>
+			</p>
+		</div>
+
+
+		
 
 	    <?php include("footer.php"); ?>
 
