7f4f0b0e18f209c6cbb8c1983412f0813ce696aa
depotoire <56601378+depotoire@users.noreply.github.com>
2020-01-07 18:23:46 +0100

    Add files via upload

--- APP/backOfficeComptes.php
+++ APP/backOfficeComptes.php
@@ -70,7 +70,7 @@
 			setcookie("action", $action, time() + 7*24*60*60);
 
 			}
-			else if ($action == "delete" || $action == "utilisateur" || $action == "gestionnaire" || $action == "admin") {//si autre action que annuler tests pour eviter erreurs
+			else if ($action == "delete" || $action == "utilisateur" || $action == "gestionnaire" || $action == "admin" || $action == "bannir") {//si autre action que annuler tests pour eviter erreurs
 
 				if($action == "delete"){//on prepare la commande pour supprimer les comptes
 
@@ -87,6 +87,9 @@
 				else if ($action == "admin") {//admin
 
 					$req = $bdd->prepare('UPDATE compte SET typeUtilisateur = 0 WHERE id = :id');
+				}
+				else if ($action == "bannir") {//bannir
+					$req = $bdd->prepare('UPDATE compte SET typeUtilisateur = -1 WHERE id = :id');
 				}
 				
 				$arrayChangements = array();
@@ -120,12 +123,28 @@
 
 				<div>
 					<?php if ($_SESSION['id'] != $user['id']) { ?>
-						<input type="checkbox" <?= 'name='.$user['id'];?> > <label> <?=$user['nom']?> <?=$user['prenom']?> <?=$user['typeUtilisateur']?> </label>
+						<input type="checkbox" <?= 'name='.$user['id'];?> >
+						<label> <?=$user['nom']?> <?=$user['prenom']?> </label>
+						<label>
+							<?php 
+							if ($user['typeUtilisateur'] == 2) {
+								echo "Admin";
+							}
+							else if ($user['typeUtilisateur'] == 1) {
+								echo "Gestionnaire";
+							}
+							else if ($user['typeUtilisateur'] == 0) {
+								echo "Utilisateur";
+							}
+							else if ($user['typeUtilisateur'] == -1) {
+								echo "Banni";
+							}?>
+						</label>
 					<?php } ?>
-					
 				</div>
-
 			<?php } ?>
+
+					
 
 
 			<br/>
@@ -133,6 +152,7 @@
 	           	<option value="utilisateur">Rendre utilisateur</option>
 	           	<option value="gestionnaire">Rendre gestionnaire</option>
 	           	<option value="admin">Rendre admin</option>
+	           	<option value="bannir">Bannir</option>
 	           	<option value="annuler">Annuler</option>
 	           	<option value="delete">Supprimer</option>
 	       	</select>
