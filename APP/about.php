c7d8be3a3a4ba31073f9ac0723f4844186ccdb1b
KynZix <56548224+KynZix@users.noreply.github.com>
2020-01-08 09:19:40 +0100

    Add files via upload

--- APP/about.php
+++ APP/about.php
@@ -12,19 +12,32 @@
 			 <?php include("header.php"); ?>
 		</header>
 	  
-	  <p> 
-	  	<div class="about">
-	  		<img src="Media/about.png">
-	  	</div>
-	  </p>
+	 <div class="titre">
 
+	  <p> 
+	  	À propos de Psytech
+	  </p>
+	</div>
 	   <p>
       	<div id="conteneur">
       		<div class="element">
       			<img src="Media/team.png" >
       		</div>
       		<div class="element">
-      			<p>Fondé à Paris en 2019, Psitech a été créé afin de répondre aux besoins des entreprises en SI.
+      			<p>Basé à Londres, Orlebar Brown a été fondé en mars 2007 avec pour mission de proposer une approche plus sophistiquée des shorts de bain pour homme. Plus qu’un short de bain, “un short avec lequel on peut se baigner”, nos shorts sont des pièces essentielles pour des vacances élégantes – quelque chose que vous pouvez porter à la plage ou au bord de la piscine, et qui est suffisamment habillé pour que vous le portiez pour boire une verre ou déjeuner.</p>
+
+
+<p>En 2010 nous avons élargi notre gamme pour aller au delà de la plage et de la piscine pour considérer les vêtements de vacances dans leur ensemble.</p>
+
+<p>Orlebar Brown a été fondé par le photographe Adam Brown, qui admet volontier que l’aventure a démarré un peu par hasard :</p>
+
+
+<p>"Nous fêtions les 40 ans d’un ami au Rajasthan. Le groupe était composé de personne du milieu du design, ou qui faisaient attention à leur apparence. Nous étions un mélange de gens mariés, célibataires, gay, hétéros, entre 25 et 55 ans, et les femmes étaient bien habillées, élégantes, tandis que les hommes…</p>
+
+<p>Ils portaient tous ces shorts de bain informes et multicolores, avec ceinture élastique, ou encore des shorts de surf baggy qui laissaient apparaître leurs fesses – ce qui est peut-être acceptable pour un jeune de 17 ans, mais pas pour un homme de plus de 30-35 ans.</p>
+
+<p>Après avoir fait une étude de marché, cherché des fournisseurs, dévelopé le site internet, créé le logo, etc. j’avais 1,000 paires de shorts de fabriqués, et j’ai commencé à les vendre depuis ma chambre. J’étais prêt à passer à autre chose, et c’était la bonne idée pour moi à ce moment-là."
+Adam Brown, Fondateur d’Orlebar Brown
 			</p>
       		</div>
       	</div>
