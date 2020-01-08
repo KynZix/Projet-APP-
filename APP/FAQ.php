45d9350ace1fcb0a5ce315247b2e2020ac2b64bd
Mecar <msaknisabri@gmail.com>
2020-01-08 16:05:53 +0100

    Update FAQ.php

--- APP/FAQ.php
+++ APP/FAQ.php
@@ -7,15 +7,15 @@
 		<link rel="stylesheet" href="CSS/faq.css">
 	</head>
 
-	<body>  
+	<body>
 		<!-- comment -->
-		
+
 
 		<body>
 			<header>
 				<?php include("header.php"); ?>
 			</header>
-			
+
 
 			<?php
 			//Connexion à la BDD
@@ -32,38 +32,8 @@
 
 			<div id="conteneur">
 				<?php
-				
-				if (isset($_GET['searchtext'])) {  //Si l'utilisateur a effectué une recherche
-					$searchquery = $_GET['searchtext'];
-					$questionsReponses = $bdd->query("SELECT * FROM faq WHERE reponse LIKE '%$searchquery%' OR reponse LIKE '%$searchquery%' ");	
-					if ($questionsReponses->rowCount() > 0) { //Si au moins 1 résultat a été trouvé	
-						while ($donnees = $questionsReponses->fetch()){?>
-							<div class="element2">
-								<!-- question -->
-								<p>
-								<strong>Question <?php echo $donnees['id']?>:</strong> <?php echo $donnees['question']?>
-								</p>
-								
-								<!-- reponses-->
-								<p>
-									<?php echo $donnees['reponse']?>
-								</p>
-								
-							</div>
-<?php 
-						}
 
-					} else { //Aucun résultat 	 ?>		
-						<p>Aucun résultat.</p>
-<?php				}  
-
-				}
-
-				//________________________________
-
-
-				else {   //L'utilisateur n'a pas effectué de recherche (soit il a directement accédé à la FAQ via le bouton du menu ou bien il a lancé une recherche vide")
-					$questionsReponses = $bdd->query("SELECT * FROM faq ORDER BY ID DESC"); //Affiche toutes les questions dans la BDD, par ordre décroissant
+				$questionsReponses = $bdd->query("SELECT * FROM faq ORDER BY ID DESC"); //Affiche toutes les questions dans la BDD, par ordre décroissant
 					while ($donnees = $questionsReponses->fetch()){?>
 						<div class="element2">
 							<!-- question -->
@@ -72,17 +42,17 @@
 								<strong>Question <?php echo $donnees['id']?>:</strong> <?php echo $donnees['question']?>
 								</p>
 							</span>
-							
+
 							<!-- reponses-->
 							<p class="text1">
 								<?php echo $donnees['reponse']?>
 							</p>
-							
+
 						</div>
-				<?php 
+				<?php
 					}
-				
-				} ?>
+
+			 ?>
 			</div>
 
 			<footer>
@@ -90,8 +60,8 @@
 			</footer>
 
 		</body>
-	
-	    
+
+
 
 
 	</body>
