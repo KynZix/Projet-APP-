df685f7f8d53ed7ee1619401190d75b8c4f462d6
Nercy99 <51016318+Nercy99@users.noreply.github.com>
2020-01-21 08:17:45 +0100

    Add files via upload

--- APP/about.php
+++ APP/about.php
@@ -15,12 +15,12 @@
 	 <div class="titre">
 
 	  <p> 
-	  	À propos de Psytech
+	  	À propos de Psitech
 	  </p>
 	</div>
 		<div class="topDesc">
 			<p>
-				Psytech est une nouvelle société basée en France et qui a été fondée en 2019. Nous avons pour but , d'aider et d'améliorer la sécurité de tous.
+				Psitech est une nouvelle société basée en France et qui a été fondée en 2019. Nous avons pour but , d'aider et d'améliorer la sécurité de tous.
 			<br/>
 				Nous proposons des tests qui sont à la fois instructifs et funs. 
 			</p>
@@ -29,17 +29,11 @@
 		</div>
 
 	<div class="botDesc">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus congue ante sit amet egestas malesuada. Sed at iaculis felis. Phasellus faucibus malesuada diam, a suscipit elit dignissim vitae. Phasellus vulputate urna at finibus pretium. Duis faucibus libero in velit laoreet gravida. Donec at nisi nibh. Mauris id orci aliquam, malesuada elit sed, viverra augue. Interdum </p>
+		<p>  </p>
 
 		<hr>
 
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
 			Il y a une citation qui nous a inspiré à créer ce site  :
 		</p>
